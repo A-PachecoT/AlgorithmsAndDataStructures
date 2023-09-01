a301c4d279fe1c2d98a9246b0d8f2fda85dad4d5
André Pacheco <apachecotaboada@gmail.com>
2023-09-01 10:09:04 -0500

    First Laboratory Sort Project on Netbeans

---
+++ Primer Laboratorio/SortingProject/src/main/java/uni/aed/sortingproject/SortingProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.sortingproject;
+
+import gui.SortJFrame;
+
+/**
+ *
+ * @author AndreP
+ */
+public class SortingProject {
+
+    public static void main(String[] args) {
+        SortJFrame v1 = new SortJFrame();
+        v1.setVisible(true);
+        
+    }
+}
---
+++ Primer Laboratorio/SortingProjects/src/main/java/uni/aed/gui/SortingProjects.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.gui;
+
+import gui.SortJFrame;
+
+/**
+ *
+ * @author AndreP
+ */
+public class SortingProjects {
+
+    public static void main(String[] args) {
+        SortJFrame v1 = new SortJFrame();
+        v1.setVisible(true);
+        
+    }
+}
